33defcb0beae16c779abe7d19c5eb43f8f83cfef
Alex Teixeira Soares dos Santos <alexsoaresdossantos27@gmail.com>
2020-12-06 12:51:14 -0300

    Exercício-1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const eplusDevList = ['Parça', 'Galego', 'Xará', 'Baqueta', 'Na', 'Lindo', 'Pablito', 'Bahia']
 
 function listDevs(list){
-
+    eplusDevList.forEach(element => {
+        console.log(element)
+    });
 }
 
 listDevs(eplusDevList)
